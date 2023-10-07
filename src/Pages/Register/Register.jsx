65b6ce4773c743fc1f42402d6d99974a8e80b3d9
Md-Nazmus-Sakib <nazmussakib0036@gmail.com>
2023-10-07 18:46:04 +0600

    login-page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import loginImg from '../../../assets/Images/Login/draw2.svg'
-import loginBgImg from '../../../assets/Images/Login/loginbg.jpg'
 import { Link } from 'react-router-dom';
+import loginImg from '../../assets/Images/Login/draw2.svg'
+import loginBgImg from '../../assets/Images/Login/loginbg.jpg'
 
-const Login = () => {
+const Register = () => {
     return (
         <div>
             <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.4)),url(${loginBgImg})`, backgroundSize: "cover" }} className="hero min-h-screen bg-base-200 mt-20 py-24 bg-opacity-50">
-                <div className="hero-content flex-col md:flex-row">
+                <div className="hero-content flex-col md:flex-row-reverse">
                     <div className="text-center lg:text-left md:w-1/2">
                         <img src={loginImg} alt="" />
                     </div>
                     <div className="card flex-shrink-0 w-full md:w-1/2 shadow-2xl bg-pink-200 bg-opacity-30 ">
-                        <h1 style={{ textShadow: ' 4px 4px 8px black' }} className="text-5xl font-bold text-center mt-12 text-white ">Login now!</h1>
+                        <h1 style={{ textShadow: ' 4px 4px 8px black' }} className="text-5xl font-bold text-center mt-12 text-white ">Register now!</h1>
                         <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text text-white font-bold">Name</span>
+                                </label>
+                                <input type="text" placeholder="name" name='name' className="input input-bordered" required />
+                            </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text text-white font-bold">Email</span>
@@ -30,10 +36,10 @@ const Login = () => {
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-secondary">Login</button>
+                                <button className="btn btn-secondary">Register</button>
                             </div>
                             <div className="text-white my-4 text-center">
-                                <h2> Don't Have an Account please <span style={{ textShadow: ' 4px 4px 8px black' }} className='text-xl bg-secondary py-2 px-4 rounded-md'><Link to={'/register'}>Register</Link></span> </h2>
+                                <h2> Already Have an Account please <span style={{ textShadow: ' 4px 4px 8px black' }} className='text-xl bg-secondary py-2 px-4 rounded-md'><Link to={'/login'}>Login</Link></span> </h2>
                             </div>
 
                         </form>
@@ -44,4 +50,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Register;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import MainLayout from "../layout/MainLayout";
 import Home from "../Pages/Home/Home/Home";
 import ServiceDetails from "../Pages/Home/Service/ServiceDetails";
 import Login from "../Pages/Home/Login/Login";
+import Register from "../Pages/Register/Register";
 
 
 
@@ -20,6 +21,11 @@ export const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login></Login>
+
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
 
             },
             {
